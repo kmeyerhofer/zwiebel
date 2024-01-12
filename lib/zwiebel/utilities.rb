dddce3df6e11f89a2e5848801d4c12d047e190be
Kurt Meyerhofer <24800059+kmeyerhofer@users.noreply.github.com>
2024-01-11 22:16:22 -0700

    WIP

--- lib/zwiebel/utilities.rb
+++ lib/zwiebel/utilities.rb
@@ -30,12 +30,12 @@ module Zwiebel
       (current_time / 60 - 1440) / 1440
     end
 
-    def self.ed25519_certificate(cert)
-      # cert_signing_key = cert.gsub("-----BEGIN ED25519 CERT-----", "").gsub("-----END ED25519 CERT-----", "").gsub("\n", "")
-      cert_signing_key = cert.gsub("-----BEGIN ED25519 CERT-----\n", "").gsub("\n-----END ED25519 CERT-----", "")#.gsub("\n", "")
-      # TODO fix this implementation
-      Base64.decode64(cert_signing_key)
-    end
+    # def self.ed25519_certificate(cert)
+    #   # cert_signing_key = cert.gsub("-----BEGIN ED25519 CERT-----", "").gsub("-----END ED25519 CERT-----", "").gsub("\n", "")
+    #   cert_signing_key = cert.gsub("-----BEGIN ED25519 CERT-----\n", "").gsub("\n-----END ED25519 CERT-----", "")#.gsub("\n", "")
+    #   # TODO fix this implementation
+    #   Base64.decode64(cert_signing_key)
+    # end
 
     def self.decrypt_layer(encrypted_data:, constant:, revision_counter:, subcredential:, blinded_key:)
       cleaned_data = encrypted_data.gsub("-----BEGIN MESSAGE-----\n", "").gsub("\n-----END MESSAGE-----", "")#.gsub("\n", "")
@@ -45,22 +45,23 @@ module Zwiebel
         # error
       end
 
-      salt = encrypted[0..SALT_LENGTH - 1]
-      ciphertext = encrypted[SALT_LENGTH..-MAC_LENGTH]
-      expected_mac = encrypted[-MAC_LENGTH..-1] # size of data is important here
+      salt = encrypted.byteslice(0, SALT_LENGTH)
+      ciphertext = encrypted.byteslice(SALT_LENGTH..-MAC_LENGTH)
+      expected_mac = encrypted.byteslice(-MAC_LENGTH..-1) # size of data is important here
 
-      # key_digest = blinded_key + subcredential + [revision_counter].pack("Q>") + salt + constant
-      key_digest = blinded_key + subcredential + [revision_counter].pack(">Q") + salt + constant
+      key_digest = blinded_key + subcredential + [revision_counter].pack("Q>") + salt + constant
+      # key_digest = blinded_key + subcredential + [revision_counter].pack(">Q") + salt + constant
       # key_digest = blinded_key + subcredential + [revision_counter].pack("v") + salt + constant
       # key_derivation_function = OpenSSL::Digest.new("SHAKE256", key_digest)
       # key_derivation_function = OpenSSL::Digest.digest("SHAKE256", key_digest)
       # keys = key_derivation_function.digest("#{S_KEY_LENGTH + S_IV_LENGTH + MAC_LENGTH}")
       bit_length = (S_KEY_LENGTH + S_IV_LENGTH + MAC_LENGTH) * 8
       keys = Shake256.new(bit_length: bit_length).digest(key_digest)
+      keys = [keys].pack("H*")
 
-      secret_key = keys[0..S_KEY_LENGTH - 1]
-      secret_iv = keys[S_KEY_LENGTH..S_KEY_LENGTH + S_IV_LENGTH - 1]
-      mac_key = keys[S_KEY_LENGTH - 1 + S_IV_LENGTH..-1]
+      secret_key = keys.byteslice(0, S_KEY_LENGTH) #[0..S_KEY_LENGTH - 1]
+      secret_iv = keys.byteslice(S_KEY_LENGTH, S_IV_LENGTH) #[S_KEY_LENGTH..S_KEY_LENGTH + S_IV_LENGTH - 1]
+      mac_key = keys.byteslice(S_KEY_LENGTH + S_IV_LENGTH, MAC_LENGTH) #[-MAC_LENGTH..-1]
 
       # mac_prefix = [mac_key.length].pack("v") + mac_key + [salt.length].pack("v") + salt
       mac_prefix = [mac_key.length].pack("Q>") + mac_key + [salt.length].pack("Q>") + salt
@@ -72,10 +73,10 @@ module Zwiebel
 
       decipher = OpenSSL::Cipher.new("aes-256-ctr")
       decipher.decrypt
+      binding.pry
       decipher.key = secret_key
       decipher.iv = secret_iv
       # plain = decipher.update(encrypted) + decipher.final
-      binding.pry
       # puts plain
 
     end
